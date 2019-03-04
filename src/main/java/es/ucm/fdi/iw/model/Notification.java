f44e3a90b400f5147645b256162e60f22bc7c07c
Guille cf <guillecf77@hotmail.com>
2019-03-04 18:09:09 +0100

    Update Notification.java
    NOTIFICATIONS

--- src/main/java/es/ucm/fdi/iw/model/Notification.java
+++ src/main/java/es/ucm/fdi/iw/model/Notification.java
@@ -3,6 +3,7 @@ package es.ucm.fdi.iw.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 @Entity
@@ -12,8 +13,8 @@ public class Notification {
 	@GeneratedValue
 	private long id;
 	
-	@OneToMany(targetEntity=User.class, mappedBy="Notification")
-	private long id_user;
+	@ManyToOne(targetEntity=User.class)
+	private User user;
 	private String text;
 	
 	
