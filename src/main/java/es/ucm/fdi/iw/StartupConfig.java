2d5a4b69868bf91da101ab2e56c2771d84e96ac8
Manuel Freire <manuel.freire@fdi.ucm.es>
2019-03-12 14:03:54 +0100

    Nueva propiedad: debug

---
+++ src/main/java/es/ucm/fdi/iw/StartupConfig.java
@@ -0,0 +1,39 @@
+package es.ucm.fdi.iw;
+
+import javax.servlet.ServletContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import es.ucm.fdi.iw.control.RootController;
+
+/**
+ * This code will execute when the application first starts.
+ * 
+ * @author mfreire
+ */
+@Component
+public class StartupConfig {
+	
+	private static final Logger log = LogManager.getLogger(RootController.class);
+	
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	private ServletContext context;
+	
+	@EventListener(ContextRefreshedEvent.class)
+	public void contextRefreshedEvent() {
+		String debugProperty = env.getProperty("es.ucm.fdi.debug");
+		context.setAttribute("debug", debugProperty != null 
+				&& Boolean.parseBoolean(debugProperty.toLowerCase()));
+		log.info("Setting global debug property to {}", 
+				context.getAttribute("debug"));
+	}
+}
