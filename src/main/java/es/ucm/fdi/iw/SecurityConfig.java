b05dc98f75c378b741698f28d232229e92568cda
Manuel Freire <manuel.freire@fdi.ucm.es>
2019-03-17 15:52:22 +0100

    allow everyone to see errors (must disable before release...)

--- src/main/java/es/ucm/fdi/iw/SecurityConfig.java
+++ src/main/java/es/ucm/fdi/iw/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	    http
 	        .authorizeRequests()
-	            .antMatchers("/css/**", "/js/**", "/img/**", "/").permitAll()
+	            .antMatchers("/css/**", "/js/**", "/img/**", "/", "/error").permitAll()
 	            .antMatchers("/clase/enter").permitAll() 		// <-- only when logging in to vote 
 	            .antMatchers("/admin**").hasRole("ADMIN")
 	            .anyRequest().authenticated()
