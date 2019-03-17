fd255e384fbd0394f460f31249ad56b85ad2b449
Manuel Freire <manuel.freire@fdi.ucm.es>
2019-03-17 15:53:37 +0100

    improved ajaxized api

--- src/main/java/es/ucm/fdi/iw/control/ApiController.java
+++ src/main/java/es/ucm/fdi/iw/control/ApiController.java
@@ -1,5 +1,11 @@
 package es.ucm.fdi.iw.control;
 
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
@@ -9,7 +15,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +24,8 @@ import org.springframework.web.util.HtmlUtils;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import es.ucm.fdi.iw.model.CGroup;
 import es.ucm.fdi.iw.model.Question;
@@ -57,22 +64,53 @@ public class ApiController {
 		log.info("Voting on q={}", qid);
 		User u = (User)session.getAttribute("u");
 		vote.setVoter(entityManager.find(User.class, u.getId()));
+		vote.setTime(new Timestamp(new Date().getTime()));
 		CGroup g = (CGroup)session.getAttribute("g");
 		Question q = entityManager.find(Question.class,  qid);
 		if (q.getGroup().getId() != g.getId()) {
 			throw new IllegalArgumentException(
 					"Tried to vote on out-of-group question");
 		}
+		vote.setQuestion(q);
+		
+		// ensure that previous votes are no longer the last one
+		@SuppressWarnings("unchecked")
+		List<Vote> last = entityManager
+			.createNamedQuery("Vote.lastByVoterAndQuestion")
+			.setParameter("userId", u.getId())
+			.setParameter("questionId", q.getId())
+			.getResultList();
+		for (Vote v : last) { 
+			log.info("No longer last: " + v.getId());
+			v.setLast(false);
+		}
+		
+		// add a date, set 'last' flag, and persist 
+		vote.setLast(true);
 		entityManager.persist(vote);
 		q.getVotes().add(vote);
-		iwSocketHandler.sendText("a", "new vote: " + vote.getValue());
+		
+		// prepare updated vote results, and notify listeners
+		@SuppressWarnings("unchecked")
+		List<Integer> values = entityManager
+			.createNamedQuery("Vote.allLastByQuestion")			
+			.setParameter("questionId", q.getId())
+			.getResultList();
+		Collections.shuffle(values); // anonymization!
+		String message = "{\"vote\": {"
+				+ "\"q_" + q.getId() + "\": " + 
+				Arrays.toString(values.toArray()) + "}}";
+		for (User p : q.getGroup().getParticipants()) {
+			iwSocketHandler.sendText(p.getLogin(), message);
+		}
 		return vote;
 	}
 	
 	@PostMapping("/q")
 	@JsonView(Views.Public.class)
 	@Transactional
-	public Question ask(Model model, @ModelAttribute Question question) {
+	public Question ask(Model model, @RequestBody Question question) 
+			 throws JsonProcessingException {
 		
 		User u = (User)session.getAttribute("u");
 		u = entityManager.find(User.class, u.getId());
@@ -87,6 +125,16 @@ public class ApiController {
 		g.getQuestions().add(question);
 		u.getQuestions().add(question);
 		
+		ObjectMapper mapper = new ObjectMapper();		
+		mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false);
+		String questionAsJson = mapper
+			      .writerWithView(Views.Public.class)
+			      .writeValueAsString(question);
+		String message = "{\"question\": "
+				+ questionAsJson + "}";
+		for (User p : g.getParticipants()) {
+			iwSocketHandler.sendText(p.getLogin(), message);
+		}		
 		return question;
 	}
 	
--- src/main/resources/static/js/kclient.js
+++ src/main/resources/static/js/kclient.js
@@ -1,9 +1,15 @@
 console.log("this is a test");
 
-function valueChanged(action, headers, input, numeric) {
-	return () => {
+function addVoteListener(e) {
+	const input = e.querySelector("input[type=range]");
+	const numeric = e.querySelector(".numeric");
+	const headers = {
+		"Content-Type": "application/json",
+		"X-CSRF-TOKEN": csrf.value
+	};
+	input.onchange = () => {
 		numeric.innerText = input.value;
-		fetch(action, {
+		fetch(e.action, {
 			method: 'POST',
 			headers: headers,
 			body: JSON.stringify({value: input.value})
@@ -11,17 +17,81 @@ function valueChanged(action, headers, input, numeric) {
 	};
 }
 
-function addVoteListener() {
-	document.querySelectorAll(".vote").forEach(e => {
-		const input = e.querySelector("input[type=range]");
-		const numeric = e.querySelector(".numeric");
-		const csrf = e.querySelector(".csrf");
-		const headers = {
-			"Content-Type": "application/json",
-			"X-CSRF-TOKEN": csrf.value
-		};
-		input.onchange = valueChanged(e.action, headers, input, numeric)
-	});
+function addQuestionListener(e) {
+	const button = e.querySelector("button");
+	const textarea = e.querySelector("textarea");
+	const headers = {
+		"Content-Type": "application/json",				
+		"X-CSRF-TOKEN": csrf.value
+	};
+	console.log("patched", e);
+	e.onsubmit = () => {
+		const body = JSON.stringify({
+			text: textarea.value, 
+			poll: document.getElementById('poll_t').checked ? 'true' : 'false' 
+		});
+		console.log("asking ", body);
+		fetch(e.action, {
+			method: 'POST',
+			headers: headers,
+			body: body				
+		}).then(response => {
+			console.log(response)
+		});
+		return false;	
+	}
 }
 
-window.addEventListener('load', addVoteListener);
+function addQuestion(data) {
+	const questionDiv = document.createElement("div");
+	questionDiv.classList.add("question");
+	questionDiv.id = "q_" + data.id;
+	questionDiv.innerHTML = [
+		'<form class="vote" th:action="/api/v/' + data.id + '" method="post">',
+		'	<div class="bars">',
+		'	<div class="me">',
+		'		<span class="barlabel">👤</span>',
+		'		<input type="range" name="vote" min="0" max="100" value="0"/>',
+		'		<span class="numeric">??</span>',
+		'	</div>',
+		'	<div class="others">',
+		'		<span class="barlabel">👥 ×0</span>',
+		'		<input disabled type="range" min="0" max="100" value="0"/>',
+		'		<span class="numeric">??</span>',
+		'	</div>',
+		'	</div>',
+		'	<span class="qtext">' + data.text + '</span>',
+		'</form>'].join('\n');	
+	document.querySelector(".question").parentElement
+		.append(questionDiv);
+	addVoteListener(questionDiv.querySelector(".vote"));
+}
+
+const ws = {
+	socket: null,
+	// llama a esto para enviar
+	send: (text) => {
+		if (ws.socket != null) {
+			ws.socket.send(text);
+		}
+	},
+	// sobreescribe esto para modificar cómo recibes cosas
+	receive: (text) => {
+		console.log(text);
+	}
+} 
+
+function addWebSocketListener() {
+	// socketUrl must be set prior to loading this script
+	if (socketUrl !== 'false') {
+		console.log("Connecting to WS '" + socketUrl + "'");
+		ws.socket = new WebSocket(socketUrl);
+		ws.socket.onmessage = (e) => ws.receive(e.data);
+	}
+}
+
+window.addEventListener('load', () => {
+	document.querySelectorAll(".vote").forEach(e => addVoteListener(e));
+	document.querySelectorAll(".ask").forEach(e => addQuestionListener(e));
+	addWebSocketListener();
+});
